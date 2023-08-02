373a41d84b8f7badca0718d749c812244f7e4738
chengyunlai <19550211498@163.com>
2023-08-02 17:53:48 +0800

    Initial commit

---
+++ src/test/java/top/chengyunlai/springbootmiddleutils/SpringBootMiddleUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package top.chengyunlai.springbootmiddleutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMiddleUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
